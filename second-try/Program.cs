97a968fa2cc9fca11762dcc5206e6523a19f3cf3
Agung Wicaksono <wicaksono.dharmayasa@taksuteknologi.com>
2022-03-20 19:26:53 +0800

    fix: add auto migrate database

--- second-try/Program.cs
+++ second-try/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -13,7 +14,15 @@ namespace second_try
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<AppDbContext>();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
